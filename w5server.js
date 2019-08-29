8a4943206f91de6cd733575567a33138c458734d
芸 <lamvoy@dyn-49-127-153-245.its.monash.edu.au>
2019-08-29 10:19:40 +1000

    initial commit

--- w5server.js
+++ w5server.js
@@ -16,12 +16,10 @@ app.use(bodyParser.urlencoded({
 app.use(express.static('img'));
 app.use(express.static('css'));
 
-//what is?
-//app.use(bodyParser.json())
 
 let db = [];
 
-let showView = __dirname+"/views/";//what is showview?
+let showView = __dirname + "/views/";
 
 app.get('/', (req, res)=>{
     res.sendFile(showView+"/index.html")
